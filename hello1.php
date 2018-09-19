7bf09febf45ebba6cabf26ab3daf62a879caf2c4
GabrielQuesadaAraya <gabrielquesadajgqa@gmail.com>
2018-09-18 20:07:38 -0600

    WORKSHOP1

---
+++ hello1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WorkShop php</title>
+</head>
+<body>
+	<center>
+		<h1><?php echo "Hello Work Shop1" ?></h1>
+<?php 
+
+echo "<td><a href='img1.jpg'>";
+echo "<img src='img1.jpg' width=600 height=400</img> ";
+
+ ?>
+ </center>	
+</body>
+</html>
